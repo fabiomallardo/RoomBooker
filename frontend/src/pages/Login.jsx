06dce7481a6efafd6a978a80ca285fd26a81d4f1
fabiomallardo <fabio.mallardo16@gmail.com>
2025-04-30 11:39:09 +0200

    modifiche

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -54,7 +54,7 @@ export default function Login() {
       localStorage.setItem("customer", JSON.stringify(data.customer));
       setTimeout(() => {
         window.location.href = "/home";
-      }, 2000);
+      }, 3000);
     } catch (err) {
       toast.error("❌ Login Google fallito");
       console.error(err);
